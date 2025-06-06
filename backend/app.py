7a9447e48a3607faf6b9b4bda84f1320527fca31
Java Sika Maulana <javamaulana0@gmail.com>
2025-06-06 22:57:55 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ FRONTEND_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 
 
 load_dotenv()
 clustering_summary_cache = ""
-app = Flask(__name__, static_folder=os.path.join('..', 'frontend'), static_url_path='')
+app = Flask(__name__, static_folder='../static')
 CORS(app)
 
 openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
@@ -114,8 +114,8 @@ def query_openrouter(prompt):
     
 
 @app.route('/')
-def index():
-    return app.send_static_file('index.html')
+def home():
+    return send_from_directory('../frontend', 'index.html')
 
 
 @app.route('/chat', methods=['POST'])
